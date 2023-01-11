8adc9c31261ebde8b7170cfde3584b10cd74c479
martin.loidolt <martin.loidolt04@gmail.com>
2023-01-11 09:45:53 +0100

    added search by first letter functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import './App.css';
-import {getMealsByName} from "./Database/apiCalls";
+import {getMealByFirstLetters, getMealsByName} from "./Database/apiCalls";
 import {Meal} from "./Utils/Interfaces";
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
     const [meals, setMeals] = useState<Meal[]>([]);
     const [textArea, setTextArea] = useState<string>("");
 
-  async function onSearchMealClick() {
+  async function onSearchMealByNameClick() {
 
       let meals = await getMealsByName(mealSearchText);
 
@@ -22,13 +22,28 @@ function App() {
       setTextArea(textAreaText);
   }
 
+    async function onSearchMealbyFirstLetters() {
+
+        let meals = await getMealByFirstLetters(mealSearchText);
+
+        setMeals(meals);
+        let textAreaText = "";
+        meals.forEach(meal => {
+            textAreaText = textAreaText + meal.strMeal + ", \n";
+        })
+
+        setTextArea(textAreaText);
+    }
+
   return (
     <div className="App">
       <div className="App-body">
-      <p>Name of Meal: </p>
+      <p>Meal Finder</p>
       <input type={"text"} onChange={event => {setMealSearchText(event.target.value)}}/>
-      <button onClick={onSearchMealClick}>Search</button>
-      <textarea readOnly={true} value={textArea}/>
+      <button onClick={onSearchMealByNameClick}>Search By Name</button>
+          <button onClick={onSearchMealbyFirstLetters}>Search By first letters</button>
+
+          <textarea readOnly={true} value={textArea}/>
       </div>
     </div>
   );
